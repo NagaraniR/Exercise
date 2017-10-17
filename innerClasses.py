c72d432cf20456ffbdae001025d80cfed8f43805
NagaraniR <32513788+NagaraniR@users.noreply.github.com>
2017-10-17 14:16:37 +0530

    Add files via upload

---
+++ innerClasses.py
@@ -0,0 +1,11 @@
+class Human:
+ def __init__(self):
+    self.name = 'Guido'
+    self.head = self.Head()
+ class Head:
+    def talk(self):
+     return 'talking'
+if __name__ == '__main__':
+    guido = Human()
+    print guido.name
+    print guido.head.talk()
